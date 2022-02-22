023eef21cfa836aaaf6a401a2b3f83a82adfe21d
maxjmar <86731281+maxjmar@users.noreply.github.com>
2022-02-22 17:42:57 -0500

    commit tree e30fbf210f04404630b0f6604c15faced05ad819

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,59 @@
 def admin_login(username, password)
-  # your code here
+  if username.downcase == "admin" && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    return "It's brisk out there!"
+  elsif temperature > 40 && temperature < 65
+    return "It's a little chilly out there!"
+  elsif temperature > 85
+    return "It's too dang hot out there!"
+  else
+    return "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 5 == 0 && num % 3 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  # if operation == '+'
+  #   num1 + num2
+  # elsif operation == '-'
+  #   num1 - num2
+  # elsif operation == '*'
+  #   num1 * num2
+  # elsif operation == '/'
+  #   num1 / num2
+  # else
+  #   puts 'Invalid operation!'
+  # end
+  
+  case operation
+  when '+'
+    num1 + num2
+  when '-'
+    num1 - num2
+  when '*'
+    num1 * num2
+  when '/'
+    num1 / num2
+  else
+    puts 'Invalid operation!'
+  end
 end
 
